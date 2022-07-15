d307a88df0cb05ba53bd398ae6b265a1170753b8
Roddymal <41775102+Roddymal@users.noreply.github.com>
2022-07-16 00:38:14 +0200

    Created Hover Car

---
+++ Dice_Drive/Assets/Scripts/HoverController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class HoverController : MonoBehaviour
+{
+    Rigidbody rb;
+    public List<GameObject> engines;
+    public GameObject propulsion;
+    public GameObject centreOfMass;
+    public float thrust = 400f;
+    public float turnSpeed = 300f;
+    public float height = 3f;
+    public float stabilization = 250f;
+    public float turnFriction = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GameObject.FindObjectOfType<Rigidbody>();
+        rb.centerOfMass = centreOfMass.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForceAtPosition(Time.deltaTime * transform.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical") * thrust, propulsion.transform.position);
+        rb.AddTorque(Time.deltaTime * transform.TransformDirection(Vector3.up) * Input.GetAxis("Horizontal") * turnSpeed);
+        foreach (GameObject engine in engines)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(engine.transform.position, transform.TransformDirection(Vector3.down), out hit, height))
+            {
+                rb.AddForceAtPosition(Time.deltaTime * transform.TransformDirection(Vector3.up) * Mathf.Pow(height - hit.distance, 2) / height * stabilization, engine.transform.position);
+            }
+            Debug.Log(hit.distance);
+        }
+        rb.AddForce(-Time.deltaTime * transform.TransformVector(Vector3.right) * transform.InverseTransformVector(rb.velocity).x * turnFriction);
+    }
+}
